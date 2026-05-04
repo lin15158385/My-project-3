d7c3df19798fae9c4b2feeffb4b959943563dd0d
lin15158385 <lin15158385@gmail.com>
2026-05-04 15:43:03 +0100

    keypad add on

--- Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -19,20 +19,19 @@ public class FirstPersonLook : MonoBehaviour
 
     void Start()
     {
-        // Lock the mouse cursor to the game screen.
-        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     void Update()
     {
-        // Get smooth velocity.
+        if (Time.timeScale == 0f) return; // 🔥 BLOQUEIA quando pausado
+
         Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
         frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
         velocity += frameVelocity;
         velocity.y = Mathf.Clamp(velocity.y, -90, 90);
 
-        // Rotate camera up-down and controller left-right from velocity.
         transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
         character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
     }
